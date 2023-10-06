bf75afd00cc40c5d6db9ed49f04a4690759fede5
jbones1000 <jwilesyoung@gmail.com>
2023-10-06 15:25:18 -0400

    making more stuff

--- lines/lines.ino
+++ lines/lines.ino
@@ -21,6 +21,11 @@ int8_t dir;
 #define YELLOW   0xFFE0 
 #define WHITE    0xFFFF
 
+struct {
+  int16_t pos[3];
+  
+} linez[32];
+
 void setup() { // Runs once on startup
   if (!display.begin()) { // Blink LED if insufficient RAM
     pinMode(LED_BUILTIN, OUTPUT);
@@ -31,6 +36,13 @@ void setup() { // Runs once on startup
   h = display.height();
   dir = 1;
   speed = 1;
+
+  for(int i=0; i<32; i++) {
+    linez[i].pos[0] = 0;
+    linez[i].pos[1] = 30;
+    linez[i].pos[2] = 60;
+  }
+  
   
 }
 
@@ -44,9 +56,33 @@ void loop() {
   }
   // Clear back framebuffer and draw circle
   display.fillScreen(0);
-  display.setColor(1, color_mixer(count,2,200));
-  display.fillCircle(count, h/2, 20, 1);
+  // draw the lines
+  for(int i=0; i<16; i++) {
+    display.setColor(1, color_mixer(255,2,200));
+    display.setColor(2, color_mixer(120,1,100));
+    display.setColor(3, color_mixer(25,0,20));
+    
+    display.drawLine(linez[i].pos[0], h/2, h-(i*20), h, 1);
+    display.drawLine(linez[i].pos[1], h/2, h-(i*20), h, 2);
+    display.drawLine(linez[i].pos[2], h/2, h-(i*20), h, 3);
+  }
   
+  for(int i=0; i<16; i++) {
+    display.setColor(1, color_mixer(255,2,200));
+    display.setColor(2, color_mixer(120,1,100));
+    display.setColor(3, color_mixer(50,0,50));
+    
+    display.drawLine(linez[i+16].pos[0], h/2, h-(i*20), 0, 1);
+    display.drawLine(linez[i+16].pos[1], h/2, h-(i*20), 0, 2);
+    display.drawLine(linez[i+16].pos[2], h/2, h-(i*20), 0, 3);
+  }
+  
+  // move the lines
+  for(int i=0; i<32; i++) {
+    linez[i].pos[2] = linez[i].pos[1];
+    linez[i].pos[1] = linez[i].pos[0];
+    linez[i].pos[0] = count;
+  }
   // Swap front/back buffers, do not duplicate current screen state to next frame,
   // we'll draw it new from scratch each time.
   display.swap();
@@ -56,3 +92,5 @@ uint16_t color_mixer(int int_r, int int_g, int int_b) {
   uint16_t mixed = ((int_r & 0xf8) << 8) + ((int_g & 0xfc) << 3) + (int_b >>3);
   return mixed;
 }
+
+
